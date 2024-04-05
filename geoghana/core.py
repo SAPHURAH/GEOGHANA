60ba4e51e9cef14469e02a7eb6f439f61016d815
jason <nftsingularity8@gmail.com>
2024-04-05 16:38:13 +0000

    create Geospatial class

--- geoghana/core.py
+++ geoghana/core.py
@@ -1,11 +1,14 @@
 import geopandas as gpd
 
-filepath = './geofiles'
+global filepath = './geofiles'
 
 class Geospatial:
     """Class for loading geo files"""
 
     def load_shape_files(filepath):
+        """Load a Shapefile and return a GeoDataFrame."""
         ...
 
-    def load_topojson(filepath)
+    def load_topojson(filepath):
+        """Load a TopoJSON file and return a GeoDataFrame."""
+        ...
