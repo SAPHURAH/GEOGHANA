12b235c402476f803ff788efafb65ef5dbd9915d
jason <nftsingularity8@gmail.com>
2024-04-05 16:20:50 +0000

    create Geospatial class

---
+++ geoghana/core.py
@@ -0,0 +1,11 @@
+import geopandas as gpd
+
+filepath = './geofiles'
+
+class Geospatial:
+    """Class for loading geo files"""
+
+    def load_shape_files(filepath):
+        ...
+
+    def load_topojson(filepath)
